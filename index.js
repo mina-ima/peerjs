96c0f6741e7b42ee9d3bcfad3f64d0ec05464aee
mina-ima <h.imanaka@mdk-j.com>
2025-08-29 08:29:03 +0900

    Initial commit with PeerJS server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const { ExpressPeerServer } = require('peer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use('/peerjs', peerServer);
+
+peerServer.on('connection', (client) => {
+  console.log('Client connected:', client.id);
+});
+
+peerServer.on('disconnect', (client) => {
+  console.log('Client disconnected:', client.id);
+});
+
+server.listen(9000, () => {
+  console.log('Express server with PeerJS is running on port 9000');
+});
+
+console.log('PeerJS server is starting...');
